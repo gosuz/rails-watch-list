32f1dcadc86cf1b7bc7bf76df32541af3c4b291b
Go Suzuki <go.suz55@gmail.com>
2024-02-16 13:54:03 +0900

    attempted fix for routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'lists/bookmarks'
+  # get 'lists/bookmarks'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root to: 'lists#index'
+  resources :lists, only: [:index, :show, :new, :create]
+
+  resources :bookmarks, only: [:new, :create, :destroy]
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 
@@ -21,8 +26,3 @@ Rails.application.routes.draw do
 
   #   resolve("Basket") { route_for(:basket) }
   # end
-  root to: 'lists#index'
-  resources :lists, only: [:index, :show, :new, :create]
-
-  resources :bookmarks, only: [:new, :create, :destroy]
-end
